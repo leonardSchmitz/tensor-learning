27a60795b14726a0e35b0bbb9d021c71c4d20b8e
Leonard Schmitz <lschmitz@math.tu-berlin.de>
2026-02-18 15:02:55 +0100

    help with plots of matrix foldings
    tensor_to_latex_folded has now an additional argument s that allows to highlight certain entires in the tensor

--- src/typesetting.jl
+++ src/typesetting.jl
@@ -8,7 +8,7 @@ function matrix_to_latex(M)
     return "\begin{bmatrix}" * body * "\end{bmatrix}"
 end
 
-function tensor_to_latex_folded(G::Array{T,3}; name="G") where T
+function tensor_to_latex_folded(G::Array{T,3}; name="G", s=0) where T
     d1, d2, d3 = size(G)
     @assert d1 == d2 == d3 "Tensor must be cubic d×d×d"
     rows_latex = String[]
@@ -17,18 +17,24 @@ function tensor_to_latex_folded(G::Array{T,3}; name="G") where T
         row_entries = []
         for k in 1:d3
             for j in 1:d2
-                push!(row_entries, string(G[i,j,k]))
+                if (i == j == k && k <= s) || 
+                   (k <= min(s, i-1, j)) || 
+                   (j <= min(s, i-1, k))
+                  push!(row_entries, "\\mathbf{"*string(G[i,j,k])*"}")
+                else 
+                  push!(row_entries, string(G[i,j,k]))
+                end 
             end
             if k < d3
-                push!(row_entries, "\vrule")  # vertical separator between slices
+                push!(row_entries, "\\vrule")  # vertical separator between slices
             end
         end
         # Join entries of this row with &
         push!(rows_latex, join(row_entries, " & "))
     end
-    body = join(rows_latex, " \\")
+    body = join(rows_latex, " \\\\")
     col_format = join(["c" for _ in 1:(d2*d3 + (d3-1))], "")
-    return "\begin{array}{" * col_format * "}" *
+    return "\\begin{array}{" * col_format * "}" *
            body *
-           "\end{array}"
+           "\\end{array}"
 end
